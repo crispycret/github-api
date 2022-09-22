f62e079c842ebc648931fb938a05e4fd6a3bb823
crispycret <bnadeau.dev@gmail.com>
2022-09-22 08:20:44 -0700

    Simple Database Setup

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,25 @@
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from config import Configuration
+
 
 app = Flask(__name__)
 
+app.config.from_object(Configuration)
 
-from . import views
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db, render_as_batch=True)
+
+
+'''
+[TODO]:
+[Population] Make a module that can make requests to the Github API and save the results in a database for faster less accurate references.
+[Retrival] Make a module that can accept requests (views) that pulls data from the database for faster reference although less accurate.
+'''
+
+
+from core import views
---
+++ core/models.py
@@ -0,0 +1,8 @@
+
+from core import db
+
+class Repo (db.Model):
+    pass
+
+class Commit (db.Model):
+    pass
