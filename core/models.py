dc3c68afc22b46d630ab811a356350372c94473b
crispycret <bnadeau.dev@gmail.com>
2022-09-23 17:24:47 -0700

    Progressed the relational database models. Added serialize to User model. Added create_multi_from_api() method for Repo. Modified models to use proper syntax and include relationships.

--- core/models.py
+++ core/models.py
@@ -3,64 +3,87 @@ from core import db
 from flask_sqlalchemy import Model
 
 class User(db.Model):
-    id = db.PrimaryKey(auto_increment=True)
-    name = db.String(128)
-    email = db.String(128)
-
-    @staticmethod
-    def create_from_api(user):
-        u = User()
-        repos = Repo.create_multi_from_api(u.get_repos())
-        return u
-
-
-class Repo (db.Model):
-    id = db.PrimaryKey(auto_increment=True)
-    user_id = db.Reference(User.id)
-    name = db.String(128)
-    created_at = db.String(128)
-    last_modified = db.String(128)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    repos = db.relationship('Repo', backref='user', lazy=True)
 
     @property
     def serialize(self):
         results = {}
+        results['id'] = self.id
+        results['name'] = self.name
+        results['email'] = self.email
+        # results['repos'] = self.repos
+        return results
+
+    @staticmethod
+    def create_from_api(user, save=False, commit=False):
+        u = User(name=user.name, email=user.email)
+
+        if (save):
+            u.save() # Check if need to save before creating repos
+            Repo.create_multi_from_api(user.get_repos(), save, False)
+            if (commit): db.commit()
+        return u
+
+
+class Repo (db.Model):
+    user_id = db.Column(db.Integer, db.ForiegnKey('user.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    html_url = db.Column(db.String(128), unique=True, nullable=False)
+    created_at = db.Column(db.String(128), unique=True, nullable=False)
+    last_modified = db.Column(db.String(128), unique=True, nullable=False)
+    commits = db.relationship('User', backref='repo', lazy=True)
+
+    @property
+    def serialize(self):
+        results = {}
+        results['user_id'] = self.user_id
+        results['id'] = self.id
         results['name'] = self.name
         results['html_url'] = self.html_url
         results['created_at'] = self.created_at
         results['last_modified'] = self.last_modified
-        results['commits'] = Commit.serialize_many(self.commits)
+        # results['commits'] = Commit.serialize_many(self.commits)
         return results
 
 
 
     @staticmethod
-    def create_from_api(repo):
-        r = Repo()
-        r.name = repo.name
-        r.html_url = repo.html_url
-        r.created_at = repo.created_at
-        r.last_modified = repo.last_modified
-        r.commits = Commit.create_multi_from_api(repo.get_commits())
+    def create_from_api(repo, save=False, commit=False):
+        r = Repo(
+            name=repo.name, html_url=repo.html_user,
+            created_at=repo.created_at, last_modified=repo.last_modified
+        )
+        if (save):
+            r.save()
+            Commit.create_multi_from_api(repo.get_commits(), save, False)
+            if (commit): db.commit()
         return r
 
 
     @staticmethod
-    def create_multi_from_api(repos):
-        return [Repo.create_from_api(r) for r in repos]
+    def create_multi_from_api(repos, save=False, commit=False):
+        results = [Repo.create_from_api(r, save, False) for r in repos]
+        if (save and commit): db.commit()
+        return results
         
 
 
 
 class Commit (db.Model):
-    id = db.PrimaryKey(auto_increment=True)
-    repo_id = db.Reference(User.id)
-    created_at = db.String(128)
-    last_modified = db.String(128)
-    pass
+    repo_id = db.Column(db.Integer, db.ForiegnKey('repo.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    created_at =  db.Column(db.String(128), unique=True, nullable=False)
+    last_modified =  db.Column(db.String(128), unique=True, nullable=False)
 
     @property
     def serialize(self):
         results={}
+        results['repo_id'] = self.repo_id
+        results['id'] = self.id
         results['created_at'] = self.created_at
         results['last_modified'] = self.last_modified
         return results
