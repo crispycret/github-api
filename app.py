027cdc3062e67f90b79c23d241e007bbe11653b4
crispycret <bnadeau.dev@gmail.com>
2022-10-14 19:12:55 -0400

    Production

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from github import Github
 from core import app
 from config import Configuration
 
+import os
+port = int(os.environ.get("PORT", 5000))
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port, debug=True)
     #app.run(ssl_context='adhoc')
 
 
