7315af9c1503e076f4ac5f96b5362fb906cfe675
crispycret <bnadeau.dev@gmail.com>
2022-11-09 18:50:24 -0500

    Modified config to replace db uri starting with postgres:// to postgresql://

--- config.py
+++ config.py
@@ -6,11 +6,16 @@ load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__)) 
 
 
+__SQLALCHEMY_DATABASE_URI__ = os.environ.get('DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'default.db')
+
+if __SQLALCHEMY_DATABASE_URI__.startswith("postgres://"):
+    __SQLALCHEMY_DATABASE_URI__ = __SQLALCHEMY_DATABASE_URI__.replace("postgres://", "postgresql://", 1)
+
 class Configuration (object):
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN")
     AUTHOR = os.environ.get("AUTHOR")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        'sqlite:///' + os.path.join(basedir, 'default.db')
+    SQLALCHEMY_DATABASE_URI = __SQLALCHEMY_DATABASE_URI__
 
     SQLALCHEMY_TRACK_MODIFICATIONS = True
